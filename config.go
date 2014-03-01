c7fea34740d4d8e5254f6f8662b6960a39e8153b
yaotian <yaotian@gmail.com>
2014-03-01 17:52:30 +0800

    conf/

--- config.go
+++ config.go
@@ -1,16 +1,16 @@
 package config
 
 import (
-	toml "github.com/pelletier/go-toml"
-	"github.com/yaotian/logs"
+    toml "github.com/pelletier/go-toml"
+    "github.com/yaotian/logs"
 )
 
 var Config *toml.TomlTree
 
 func init() {
-	config_toml, err := toml.LoadFile("config.toml")
-	if err != nil {
-		logs.Logger.Error("Error ", err.Error())
-	}
-	Config = config_toml
+    config_toml, err := toml.LoadFile("conf/config.toml")
+    if err != nil {
+        logs.Logger.Error("Error ", err.Error())
+    }
+    Config = config_toml
 }
